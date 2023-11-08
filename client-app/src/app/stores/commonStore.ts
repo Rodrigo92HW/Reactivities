a4ba57dae88aa1866dc5b88b5be1fc4d7dd1e6e4
Huevo <84205027+Rodrigo92HW@users.noreply.github.com>
2023-11-08 11:46:21 -0300

    Lesson 13

--- client-app/src/app/stores/commonStore.ts
+++ client-app/src/app/stores/commonStore.ts
@@ -1,14 +1,35 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, reaction } from "mobx";
 import { ServerError } from "../models/serverError";
 
 export default class commonStore {
     error: ServerError | null = null;
+    token: string | null | undefined = localStorage.getItem('jwt');
+    appLoaded: boolean = false;
 
     constructor() {
         makeAutoObservable(this);
+
+        reaction(
+            () => this.token,
+            token => {
+                if(token) {
+                    localStorage.setItem('jwt', token)
+                } else {
+                    localStorage.removeItem('jwt')
+                }
+            }
+        )
     }
 
     setSeverError(error: ServerError) {
         this.error = error;
+    }
+
+    setToken = (token: string | null | undefined) => {
+        this.token = token;
+    }
+
+    setAppLoaded = () => {
+        this.appLoaded = true;
     }
 }
--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -1,15 +1,21 @@
 import { createContext, useContext } from "react";
 import ActivityStore from "./activityStore";
 import commonStore from "./commonStore";
+import UsersStore from "./userStore";
+import ModalStore from "./modalStore";
 
 interface Store {
     activityStore: ActivityStore;
-    commonStore: commonStore
+    commonStore: commonStore;
+    userStore: UsersStore;
+    modalStore: ModalStore
 }
 
 export const store: Store = {
     activityStore: new ActivityStore(),
-    commonStore: new commonStore()
+    commonStore: new commonStore(),
+    userStore: new UsersStore(),
+    modalStore: new ModalStore()
 }
 
 export const StoreContext = createContext(store);
