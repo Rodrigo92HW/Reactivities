4906c692bfcdce3e05d13dd488c2410d98f0597b
Huevo <84205027+Rodrigo92HW@users.noreply.github.com>
2024-01-24 16:48:16 -0300

    Lesson 20

--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -3,18 +3,23 @@ import ProfilePhotos from "./ProfilePhotos";
 import { Profile } from "../../app/models/profile";
 import { observer } from "mobx-react-lite";
 import ProfileAbout from './ProfileAbout';
+import ProfileFollowing from "./ProfileFollowing";
+import { useStore } from "../../app/stores/store";
 
 interface Props {
     profile: Profile
 }
 
 export default observer(function ProfileContent({profile}: Props) {
+
+    const {profileStore} = useStore();
+
     const panes = [
         { menuItem: 'About', render: () => <ProfileAbout /> },
         {menuItem: 'Photos', render: () => <ProfilePhotos profile={profile}/>},
         {menuItem: 'Event', render: () => <Tab.Pane>Event Content</Tab.Pane>},
-        {menuItem: 'Followers', render: () => <Tab.Pane>Followers Content</Tab.Pane>},
-        {menuItem: 'Following', render: () => <Tab.Pane>Following Content</Tab.Pane>}
+        {menuItem: 'Followers', render: () => <ProfileFollowing/>},
+        {menuItem: 'Following', render: () => <ProfileFollowing/>},
     ];
 
     return (
@@ -22,6 +27,7 @@ export default observer(function ProfileContent({profile}: Props) {
             menu={{fluid: true, vertical: true}}
             menuPosition="right"
             panes={panes}
+            onTabChange={(_, data) => profileStore.setActiveTab(data.activeIndex as number)}
         />
     )
 })
